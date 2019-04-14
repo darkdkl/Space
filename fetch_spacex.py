df99322a485e3a4bf6126e93c35a275e2de2d5d6
Dark_Dmake <dark.dmake@gmail.com>
2019-04-14 23:48:59 +0300

    перенес номер запуска в переменную

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -2,7 +2,8 @@ import requests,os
 import main
 
 def fetch_spacex_last_launch(name):
-    response = requests.get('https://api.spacexdata.com/v3/launches/76')
+    launch='76'
+    response = requests.get(f'https://api.spacexdata.com/v3/launches/{launch}')
     url_imgs = []
     for img_url_prepare in response.json()['links']["flickr_images"]:
         url_imgs.append(img_url_prepare)
