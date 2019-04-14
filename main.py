c0f38d9c4842e49bc9a8f5851ab8f5219c9b286c
Dark_Dmake <dark.dmake@gmail.com>
2019-04-14 21:49:55 +0300

    разделение на модули

--- main.py
+++ main.py
@@ -1,42 +1,22 @@
 import requests,os,time
 from instabot import Bot
 from dotenv import load_dotenv
+import fetch_spacex
+import fetch_hubble
 
 PATH = os.path.normcase('./images')
 os.makedirs(PATH, exist_ok=True)
 
-
-def fetch_spacex_last_launch(name):
-    response = requests.get('https://api.spacexdata.com/v3/launches/76')
-    url_imgs = []
-    for img_url_prepare in response.json()['links']["flickr_images"]:
-        url_imgs.append(img_url_prepare)
-
-    for number, url_img in enumerate(url_imgs):
-        names = f'{name}{number}'
-        download_image(url_img, names)
 
 
 def get_extension(url):
     return url.rsplit('.')[-1]
 
 
-def fetch_hubble(collection):
-    params = {'page': 'all'}
-    response = requests.get(
-        f'http://hubblesite.org/api/v3/images/{collection}', params=params)
-    img_url_prepare = []
-    for img_id in response.json():
-        ids = img_id['id']
-        response = requests.get(f'http://hubblesite.org/api/v3/image/{ids}')
-
-        for img in response.json()['image_files']:
-            img_url_prepare.append(img['file_url'])
-            download_image(img_url_prepare[-1], ids)
 
 
 def download_image(url, name):
-
+    print('\u001b[1A загрузка файла',name ,'пожалуйста подождите...')
     img_response = requests.get(url)
     exstension = get_extension(url)
     fname = f'{PATH}/{name}.{exstension}'
@@ -45,10 +25,11 @@ def download_image(url, name):
 
 def main():
     
-    fetch_hubble('wallpaper')
-    fetch_spacex_last_launch('spacex')
+    fetch_hubble.fetch_hubble('wallpaper')
+    fetch_spacex.fetch_spacex_last_launch('spacex')
     bot = Bot()
     bot.login(username=os.getenv('LOGIN'), password=os.getenv('PASSWORD'))
+    print('Начинаем выгрузку файлов в Instagram  ,пожалуйста подождите...')
     extension=('.jpg','.JPG','.png','.PNG')
     for img in os.listdir(PATH):
         if os.path.isfile(PATH+'/'+img) and img.endswith(extension):
@@ -61,4 +42,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
 
