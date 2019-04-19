60d9825d8392f6430e248b9a5547b8cc1363e113
Dark_Dmake <dark.dmake@gmail.com>
2019-04-19 23:26:28 +0300

    дописал функцию проверяющую наличие фотографий при запуске

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -1,28 +1,34 @@
 import requests,os
-
-PATH = os.path.normcase('./images')
-os.makedirs(PATH, exist_ok=True)
-
-def get_extension(url):
-    return url.rsplit('.')[-1]
-
-def download_image(url, name):
-    print('\u001b[1A загрузка файла с именем:',name ,',пожалуйста подождите...')
-    img_response = requests.get(url)
-    exstension = get_extension(url)
-    fname = f'{PATH}/{name}.{exstension}'
-    with open(fname, 'wb') as img:
-        img.write(img_response.content)
-
-
+import download_img
 
 def fetch_spacex_last_launch(name):
-    launch='76'
+    launch=check_img_last_launch_availability()
     response = requests.get(f'https://api.spacexdata.com/v3/launches/{launch}')
     url_imgs = []
     for img_url_prepare in response.json()['links']["flickr_images"]:
         url_imgs.append(img_url_prepare)
 
     for number, url_img in enumerate(url_imgs):
-        names = f'{name}{number}'
-        download_image(url_img, names)
+        name = f'{name}{number}'
+        download_img.download_image(url_img, name)
+
+def check_img_last_launch_availability():
+
+    latest_launch_url='https://api.spacexdata.com/v3/launches/latest'
+    response = requests.get(latest_launch_url)
+    number_of_flight=int(response.json()["flight_number"])
+    
+    if not bool(response.json()['links']["flickr_images"]):
+        launch_with_photo=0
+        while True:
+            launch_with_photo=number_of_flight-1
+            search = requests.get(f'https://api.spacexdata.com/v3/launches/{launch_with_photo}')
+            if bool(search):
+                break
+        return launch_with_photo
+    return number_of_flight
+
+
+if __name__ == "__main__":
+    image_name='spacex_'    
+    fetch_spacex_last_launch(image_name)
