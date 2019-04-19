18ee423af13551af1a75b4b5d5a35ce2c137a0f8
Dark_Dmake <dark.dmake@gmail.com>
2019-04-19 23:26:51 +0300

    мелкие правки

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
-import requests,os,time
+import requests
+import os
+import time
 from instabot import Bot
 from dotenv import load_dotenv
 import fetch_spacex
 import fetch_hubble
+import download_img
+import settings
 
-PATH = os.path.normcase('./images')
-os.makedirs(PATH, exist_ok=True)
 
 def main():
-    
-    fetch_hubble.fetch_hubble()
+
+    fetch_hubble.fetch_hubble_images()
     fetch_spacex.fetch_spacex_last_launch('spacex')
-    caption='КосмоФото'
+    caption = 'КосмоФото'
     bot = Bot()
     bot.login(username=os.getenv('LOGIN'), password=os.getenv('PASSWORD'))
     print('Начинаем выгрузку файлов в Instagram  ,пожалуйста подождите...')
-    extension=('.jpg','.JPG','.png','.PNG')
-    for img in os.listdir(PATH):
-        if os.path.isfile(PATH+'/'+img) and img.endswith(extension):
-            bot.upload_photo(PATH+'/'+img, caption=caption)
+    extensions=('.jpg','.JPG','.png','.PNG')
+    for img in os.listdir(settings.IMAGE_DIR):
+        if os.path.isfile(settings.IMAGE_DIR+'/'+img) and img.endswith(extensions):
+            bot.upload_photo(settings.IMAGE_DIR+'/'+img, caption=caption)
             time.sleep(3)
         else:
             print('Подходящие файлы не найдены')
 
 
-
 if __name__ == "__main__":
     main()
-    
-
